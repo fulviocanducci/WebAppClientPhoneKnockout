9e94edde08d438272017708a2a96a07fb5d536af
fulviocanducci <fulviocanducci@hotmail.com>
2025-11-27 18:03:42 -0300

    add

--- WebAppClientPhone/Controllers/HomeController.cs
+++ WebAppClientPhone/Controllers/HomeController.cs
@@ -21,5 +21,10 @@ namespace WebAppClientPhone.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Select()
+        {
+            return View();
+        }
     }
 }
--- WebAppClientPhone/wwwroot/js/site.js
+++ WebAppClientPhone/wwwroot/js/site.js
@@ -16,7 +16,71 @@ function getVerificationToken() {
    return $('input[name ="__RequestVerificationToken"]').val();
 }
 
-function select2(name, label, url = null, width = '100%', dropdownParent = null, minimumInputLength = 2) {
+function debounce(fn, delay) {
+   let timer = null;
+   return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => fn.apply(this, args), delay);
+   };
+}
+
+function tomSelect2(name, url, valueField = 'id', labelField = 'text', searchField = 'text', loadThrottle = 400) {
+   function queryAjax(url, query, callback) {
+      const params = "?q=" + query + "&term=" + query + "&__RequestVerificationToken=" + getVerificationToken();
+      fetch(url + params, {         
+         method: 'POST'
+      })
+         .then(res => res.json())
+         .then(callback)
+         .catch(() => callback());
+   }
+   new TomSelect(name, {
+      valueField: valueField,
+      labelField: labelField,
+      searchField: searchField,
+      loadThrottle: loadThrottle,
+      persist: true,
+      load: function (query, callback) {
+         queryAjax(url, query, callback);
+      },
+      onItemAdd: function (value, data) {
+         console.log('onItemAdd', value, data);
+      }
+   });
+}
+
+function selectize(name, url, valueField = 'id', labelField = 'text', searchField = 'text', loadThrottle = 400) {
+   $(name).selectize({
+      valueField: valueField,
+      labelField: labelField,
+      searchField: searchField,
+      preload: false,
+      loadThrottle: loadThrottle,
+      load: function (query, callback) {
+         if (!query.length) {
+            return callback();
+         }
+         $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+               q: query,
+               term: query,
+               __RequestVerificationToken: getVerificationToken()
+            },
+            error: function () {
+               callback();
+            },
+            success: function (res) {
+               callback(res.results);
+            }
+         });
+      }
+   });
+}
+
+function select2(name, label, url = null, width = '100%', dropdownParent = null, minimumInputLength = 1) {
    let base = {
       placeholder: label,
       allowClear: true,
@@ -56,7 +120,6 @@ function select2(name, label, url = null, width = '100%', dropdownParent = null,
    }
    $(name).select2(base);
 }
-
 
 function getJSONPhoneByClientId(value, fn) {
    $.getJSON('/clients/phones/client-' + value, fn);
